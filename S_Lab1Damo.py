843a0163d0f83e97a8fa9e4c48adbe5854b09529
ekdus9968 <kan.seyoung1018@gmail.com>
2025-09-22 14:57:50 -0400

    new maze test

--- S_Lab1Damo.py
+++ S_Lab1Damo.py
@@ -23,23 +23,23 @@ def move_str(bot, D, max_v = 50):
     initial_r = bot.get_right_encoder_reading()
     
     while True:
-        # 현재 이동 거리 계산
+        # Curr D
         l_delta = bot.get_left_encoder_reading() - initial_l
         r_delta = bot.get_right_encoder_reading() - initial_r
         distance_traveled = wheel_radius * (l_delta + r_delta) / 2
         
-        # 디버깅 출력
-        print("Motor Encoder Readings: ", bot.get_encoder_readings())
-        print("Distance traveled: ", distance_traveled, '\n')
+        # print
+        print("STR:: Motor Encoder Readings: ", bot.get_encoder_readings())
+        print("STR:: Distance traveled: ", distance_traveled, '\n')
         
-        # 목표 거리 도달 시 멈춤
+        # stop 
         if distance_traveled >= D:
             bot.set_left_motor_speed(0)
             bot.set_right_motor_speed(0)
             bot.stop_motors()
             break
         
-        time.sleep(0.01)  # 루프 너무 빠르게 도는 것 방지
+        time.sleep(0.01)  # prevent loop susper fast
         
 #DONE!
 def move_rot(bot, delta_theta, max_v=50):
@@ -49,7 +49,7 @@ def move_rot(bot, delta_theta, max_v=50):
     new_yaw: 목표 회전 각도 (rad)
     max_v: 회전 속도 (-100 ~ 100)
     """
-    # 회전 방향 계산
+    # cal Orientation
     l_dist = -delta_theta * (axel_length / 2)
     r_dist = delta_theta * (axel_length / 2)
     
@@ -60,11 +60,11 @@ def move_rot(bot, delta_theta, max_v=50):
     v_l = max_v * l_dist / max_dist
     v_r = max_v * r_dist / max_dist
     
-    # 모터 속도 설정
+    # speed
     bot.set_left_motor_speed(v_l)
     bot.set_right_motor_speed(v_r)
     
-    # 단순 루프: 목표 각도에 도달하면 정지
+    # loop
     while True:
         
         l_delta = bot.get_left_encoder_reading() - init_l
@@ -80,7 +80,7 @@ def move_rot(bot, delta_theta, max_v=50):
 
 "Turning R/W"
 def move_arc(bot, R, theta, direction="CCW", max_v=50):
-    # 왼쪽/오른쪽 이동 거리 계산
+    # cal r/l turn
     if direction.upper() == "CCW":
         d_left = (R - axel_length/2) * theta
         d_right = (R + axel_length/2) * theta
@@ -105,7 +105,7 @@ def move_arc(bot, R, theta, direction="CCW", max_v=50):
         d_l = wheel_radius * l_delta
         d_r = wheel_radius * r_delta
         
-        #each loop. cal rest of D ratio in live 
+        #each loop, cal rest of D ratio in live 
         #when i only use ratio var, it stops faster
         l_remain = d_left - d_l
         r_remain = d_right - d_r
@@ -136,30 +136,12 @@ P = [(1.0, -2.0, np.pi),
      (-1.0, -0.5, 3*np.pi/2),
      (-0.5, -1.0, 7*np.pi/4)]
 
-# ---- 자동 주행 루프 ----
-# for i in range(len(P)-1):
-#     start = P[i][:2]
-#     end = P[i+1][:2]
-    
-#     # 이동 벡터
-#     vec = np.array(end) - np.array(start)
-#     distance = np.linalg.norm(vec)
-    
-#     # 목표 각도
-#     angle_target = np.arctan2(vec[1], vec[0])
-#     angle_current = P[i][2]
-#     delta_angle = (angle_target - angle_current + np.pi) % (2*np.pi) - np.pi
-    
-#     # 회전 후 직선 이동
-#     move_rot(bot, angle_current, angle_target)
-#     move_str(bot, distance, max_v=50)
-
-
+#Old maze
 #D_01 = np.linalg.norm(np.array(P[1][:2]) - np.array(P[0][:2]))
 #move_str(bot, D=0.3, max_v=50)  # 50 ~100 단위로 속도 조정
 
 # # P1 -> P2
-move_arc(bot, R=1.0, theta=np.pi, direction="CW", max_v=50)
+#move_arc(bot, R=1.0, theta=np.pi, direction="CW", max_v=50)
 
 # # P2 -> P3
 # D_23 = np.linalg.norm(np.array(P[3][:2]) - np.array(P[2][:2]))
@@ -178,4 +160,21 @@ move_arc(bot, R=1.0, theta=np.pi, direction="CW", max_v=50)
 
 # print("Path 완료!")
 # bot.stop_motors()
+
+# NEW MAZE 
+#P0->P1
+move_str(bot, D=0.5, max_v=50) 
+#P1->P1
+move_rot(bot, np.pi / 4, max_v=50)
+#P1->P2
+move_str(bot, D=0.5, max_v=50) 
+#P2->P2
+move_rot(bot, - np.pi / 4, max_v=50)
+#P2->P3
+move_str(bot, D=0.5, max_v=50) 
+#P3->P3
+move_rot(bot, - np.pi / 2, max_v=50)
+#P3->P4
+move_arc(bot, R=0.5, theta=np.pi / 2 , direction="CW", max_v=50)
+
 
