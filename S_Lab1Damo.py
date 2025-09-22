979c61f081363fd3133dc65cc95fa8f313ea188d
ekdus9968 <kan.seyoung1018@gmail.com>
2025-09-22 14:16:14 -0400

    fix error

--- S_Lab1Damo.py
+++ S_Lab1Damo.py
@@ -113,6 +113,8 @@ def move_arc(bot, R, theta, direction="CCW", max_v=50):
         d_r = wheel_radius * r_delta
         
         if abs(d_l) >= abs(d_left) or abs(d_r) >= abs(d_right):
+            bot.set_left_motor_speed(0)
+            bot.set_right_motor_speed(0)
             bot.stop_motors()
             break
     time.sleep(0.01)       
