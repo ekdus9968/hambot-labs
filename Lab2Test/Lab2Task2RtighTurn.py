af1eed31b8f57842464c7d4f516a8b7d42561228
ekdus9968 <kan.seyoung1018@gmail.com>
2025-10-16 16:03:25 -0400

    add error bound

--- Lab2Test/Lab2Task2RtighTurn.py
+++ Lab2Test/Lab2Task2RtighTurn.py
@@ -86,9 +86,9 @@ def rotate(bot, radianAngle):
         delta = (target_yaw - current_yaw + 540) % 360 - 180
 
         print(f"Rotate:: current={current_yaw:.2f}, target={target_yaw:.2f}, delta={delta:.2f}")
-
+        error = target_yaw - delta
         # 목표 각도에 거의 도달하면 정지
-        if abs(delta) < 5.0:  # ±2 허용 오차
+        if abs(error) < 5.0:  # ±2 허용 오차
             bot.stop_motors()
             print("Rotation complete.")
             break
