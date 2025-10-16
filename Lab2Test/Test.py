4cb515c36f6611c9b6dfaeb2c6a2254fbadd715c
ekdus9968 <kan.seyoung1018@gmail.com>
2025-10-16 16:45:25 -0400

    one more condition d_r>1.2

--- Lab2Test/Test.py
+++ Lab2Test/Test.py
@@ -54,7 +54,7 @@ def resetPID(bot):
 # ========================
 # move_forward
 # ========================
-def move_forward(bot, speed=10, duration=2.0):
+def move_forward(bot, speed=30, duration=2.0):
     """Move straight forward for 'duration' seconds"""
     print("Moving forward...")
     bot.set_left_motor_speed(speed)
@@ -181,6 +181,12 @@ def withWall(bot):
                 rotate(bot, math.pi / 2)
                 move_forward(bot)
                 break
+        elif D_r > 1.2:
+            print(f"[BEFORETURN] D_f={D_f:.4f}, E_f={E_f:.4f}, D_r={D_r:.4f}, E_r={E_r:.4f}, control={control:.4f}, D_l={D_l:.4f}")
+            print("Right wall is far from at least 1.2")
+            rotate(bot, math.pi / 2)
+            move_forward(bot)
+            
         time.sleep(dt)
         #Turing 
         # if E_f < 0.5 and (D_r > D_l):
