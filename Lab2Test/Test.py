6a2fb351afacf4449429471cf2762b1c0cee15af
ekdus9968 <kan.seyoung1018@gmail.com>
2025-10-16 20:37:28 -0400

    arc

--- Lab2Test/Test.py
+++ Lab2Test/Test.py
@@ -304,7 +304,7 @@ def withWall(bot):
 
             # 센서 데이터 (degrees 기준)
             D_f = np.nanmin(lidar[179:181])  / 600 # front
-            D_r = np.nanmin(lidar[265:285])  / 600 # right
+            D_r = np.nanmax(lidar[265:285])  / 600 # right
             D_l = np.nanmin(lidar[75:105])   / 600 # left
 
             # 결측치 처리
@@ -350,13 +350,13 @@ def withWall(bot):
                 if D_r < D_l:
                     print("LEFT:::STOPSTOPSTOPSTOPSTOPSTOPSTOSPTOSPTOPSTOPSTOSPTOPOSP")
                     #rotate(bot, -math.pi / 2)
-                    move_arc(bot, R = 0.2, theta = np.pi / 2, direction="CCW", max_v=50)
+                    move_arc(bot, R = 0.1, theta = np.pi / 2, direction="CCW", max_v=50)
                     move_forward(bot)
                     break
                 elif D_r > D_l:
                     print("Right:::STOPSTOPSTOPSTOPSTOPSTOPSTOSPTOSPTOPSTOPSTOSPTOPOSP")
                     #rotate(bot, math.pi / 2)
-                    move_arc(bot, R = 0.2, theta = np.pi / 2, direction="CW", max_v=50)
+                    move_arc(bot, R = 0.1, theta = np.pi / 2, direction="CW", max_v=50)
                     move_forward(bot)
                     break
             elif D_r > 1.2:
@@ -365,7 +365,7 @@ def withWall(bot):
                 bot.set_right_motor_speed(0)
                 print(f"[BEFORETURN] D_f={D_f:.4f}, E_f={E_f:.4f}, D_r={D_r:.4f}, E_r={E_r:.4f}, control={control:.4f}, D_l={D_l:.4f}")
                 print("Right wall is far from at least 1.2")
-                rotate(bot, math.pi / 2)
+                move_arc(bot, R = 0.1, theta = np.pi / 2, direction="CW", max_v=50)
                 move_forward(bot)
                 
             time.sleep(dt)
