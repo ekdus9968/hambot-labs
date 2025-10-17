ef1f4772c4ae4139253d2a7ad86ce06846ca0d6b
ekdus9968 <kan.seyoung1018@gmail.com>
2025-10-16 22:27:42 -0400

    plzzzz

--- Lab2Test/Test.py
+++ Lab2Test/Test.py
@@ -56,7 +56,7 @@ def resetPID(bot):
 # ========================
 # move_forward
 # ========================
-def move_forward(bot, speed=7, duration=4):
+def move_forward(bot, speed=10, duration=4):
     """Move straight forward for 'duration' seconds"""
     print("Moving forward...")
     bot.set_left_motor_speed(speed)
