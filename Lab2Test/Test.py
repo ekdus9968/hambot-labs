054bc32e40e0351d067ca8d78e091e4c2468b222
ekdus9968 <kan.seyoung1018@gmail.com>
2025-10-16 22:52:36 -0400

    100

--- Lab2Test/Test.py
+++ Lab2Test/Test.py
@@ -400,13 +400,13 @@ def withWall(bot):
                 bot.set_right_motor_speed(0)
                 if D_r < D_l:
                     print("LEFT:::STOPSTOPSTOPSTOPSTOPSTOPSTOSPTOSPTOPSTOPSTOSPTOPOSP")
-                    rotate(bot, np.deg2rad(115) / 2)
+                    rotate(bot, np.deg2rad(100) )
                     #move_arc(bot, R = 0.2, theta = np.pi , direction="CCW", max_v=15)
                     move_forward(bot)
                     break
                 elif D_r > D_l:
                     print("Right:::STOPSTOPSTOPSTOPSTOPSTOPSTOSPTOSPTOPSTOPSTOSPTOPOSP")
-                    rotate(bot,np.deg2rad(115) / 2)
+                    rotate(bot,np.deg2rad(100) )
                     #move_arc(bot, R = 0.2, theta = np.pi , direction="CW", max_v=15)
                     move_forward(bot)
                     break
@@ -417,7 +417,7 @@ def withWall(bot):
                 print(f"[BEFORETURN] D_f={D_f:.4f}, E_f={E_f:.4f}, D_r={D_r:.4f}, E_r={E_r:.4f}, control={control:.4f}, D_l={D_l:.4f}")
                 print("Right wall is far from at least 1.2")
                 #move_arc(bot, R = 0.2, theta = np.pi , direction="CW", max_v=10)
-                rotate(bot, -np.deg2rad(100) / 2)
+                rotate(bot, -np.deg2rad(100) )
                 move_forward(bot)
                 
             time.sleep(dt)
