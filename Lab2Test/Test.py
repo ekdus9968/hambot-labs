642ef6cebfe681638418977fd33c511b16fdd7d5
ekdus9968 <kan.seyoung1018@gmail.com>
2025-10-16 21:18:30 -0400

    move forward

--- Lab2Test/Test.py
+++ Lab2Test/Test.py
@@ -56,7 +56,7 @@ def resetPID(bot):
 # ========================
 # move_forward
 # ========================
-def move_forward(bot, speed=20, duration=2.0):
+def move_forward(bot, speed=10, duration=1.0):
     """Move straight forward for 'duration' seconds"""
     print("Moving forward...")
     bot.set_left_motor_speed(speed)
@@ -362,13 +362,13 @@ def withWall(bot):
                     print("LEFT:::STOPSTOPSTOPSTOPSTOPSTOPSTOSPTOSPTOPSTOPSTOSPTOPOSP")
                     #rotate(bot, -math.pi / 2)
                     move_arc(bot, R = 0.2, theta = np.pi , direction="CCW", max_v=10)
-                    # move_forward(bot)
+                    move_forward(bot)
                     break
                 elif D_r > D_l:
                     print("Right:::STOPSTOPSTOPSTOPSTOPSTOPSTOSPTOSPTOPSTOPSTOSPTOPOSP")
                     #rotate(bot, math.pi / 2)
                     move_arc(bot, R = 0.2, theta = np.pi , direction="CW", max_v=10)
-                    # move_forward(bot)
+                    move_forward(bot)
                     break
             elif D_r > 0.6:
                 bot.stop_motors()
@@ -377,7 +377,7 @@ def withWall(bot):
                 print(f"[BEFORETURN] D_f={D_f:.4f}, E_f={E_f:.4f}, D_r={D_r:.4f}, E_r={E_r:.4f}, control={control:.4f}, D_l={D_l:.4f}")
                 print("Right wall is far from at least 1.2")
                 move_arc(bot, R = 0.2, theta = np.pi , direction="CW", max_v=10)
-                # move_forward(bot)
+                move_forward(bot)
                 
             time.sleep(dt)
         #Turing 
