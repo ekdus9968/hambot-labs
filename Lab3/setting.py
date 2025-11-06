9e0f0ce920ac6c570f5a73ab118f1d2c068f52ad
ekdus9968 <kan.seyoung1018@gmail.com>
2025-11-05 22:07:00 -0500

    again

--- Lab3/setting.py
+++ Lab3/setting.py
@@ -279,8 +279,9 @@ class BUG0:
             elif self.state == 'go_close':
                 print("~~~~~~~~~GO CLOSER~~~~~~~~~~")
                 if self.front_dist> 250:
-                    self.bot.set_left_motor_speed(2.0) / self.count
-                    self.bot.set_right_motor_speed(2.0) / self.count
+                    speed = 2.0 / self.count
+                    self.bot.set_left_motor_speed(speed) 
+                    self.bot.set_right_motor_speed(speed) 
                 else:
                     self.change_state('go close to check in goal')
                     self.state = 'check_in_goal'
@@ -288,8 +289,9 @@ class BUG0:
             elif self.state == 'go_far':
                 print("~~~~~~~~~GO FAR~~~~~~~~~~")
                 if self.front_dist  < 250:
-                    self.bot.set_left_motor_speed(-2.0) / self.count
-                    self.bot.set_right_motor_speed(-2.0) / self.count
+                    speed = 2.0 / self.count
+                    self.bot.set_left_motor_speed(-speed) 
+                    self.bot.set_right_motor_speed(-speed)
                 else:
                     self.change_state('go far to check in goal')
                     self.state = 'check_in_goal'
