276ee66a17d616b4125cac002817674e847b052c
ekdus9968 <kan.seyoung1018@gmail.com>
2025-11-07 11:20:43 -0500

    detect file start

--- Lab3/detect_landmark.py
+++ Lab3/detect_landmark.py
@@ -276,7 +276,7 @@ class BUG0:
     # -------------------------------
     def run_state(self):
         self.update_position_and_distance()
-        self.change_state('turn_to_wall')
+        self.change_state('start')
 
         # goal angle 한 번만 계산
         self.goal_angle = self.calculate_goal_angle()
